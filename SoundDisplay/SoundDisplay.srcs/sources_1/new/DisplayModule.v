1c3a48adc880cecb0579c02bfca18ccd0b3a52fb
ng-andre <ng.andr3@gmail.com>
2021-03-18 16:29:10 +0800

    V3 Combine Test

--- SoundDisplay/SoundDisplay.srcs/sources_1/new/DisplayModule.v
+++ SoundDisplay/SoundDisplay.srcs/sources_1/new/DisplayModule.v
@@ -21,8 +21,9 @@
 
 
 module DisplayModule(
-    input clk6p25m,  
-    output cs, sdin, sclk, d_cn, resn, vccen, pmoden//output of OLED display
+    input clk6p25m,
+    output cs, sdin, sclk, d_cn, resn, vccen, pmoden,//output of OLED display
+    input border_switch
     );
     
     reg [15:0] colour_data;
@@ -35,16 +36,32 @@ module DisplayModule(
     wire frame_begin, sending_pixels, sample_pixel, teststate;
 
     always @ (posedge clk6p25m) begin    
-        if ((pixel_index % 96) == 0)
-             oled_data <= 16'b1111111111111111;//border_colour;
-        else if ((pixel_index % 96) == 95)
-             oled_data <= 16'b1111111111111111;// border_colour;
-        else if ((pixel_index / 96) == 0)
-             oled_data <= 16'b1111111111111111;// border_colour;
-        else if ((pixel_index / 96) == 63)
-             oled_data <= 16'b1111111111111111;// border_colour;       
-         else 
-            oled_data <= 16'b0;
+        if (border_switch) begin //3 pixel wide border
+        
+            if ((pixel_index % 96) == 0 || (pixel_index % 96) == 1 || (pixel_index % 96) == 2)
+                 oled_data <= 16'b1111111111111111;//border_colour;
+            else if ((pixel_index % 96) == 95 || (pixel_index % 96) == 94 || (pixel_index % 96) == 93)
+                 oled_data <= 16'b1111111111111111;// border_colour;
+            else if ((pixel_index / 96) == 0 || (pixel_index / 96) == 1 || (pixel_index / 96) == 2)
+                 oled_data <= 16'b1111111111111111;// border_colour;
+            else if ((pixel_index / 96) == 63 || (pixel_index / 96) == 62 || (pixel_index / 96) == 61)
+                 oled_data <= 16'b1111111111111111;// border_colour;       
+            else 
+                oled_data <= 16'b0;
+        end
+        else begin
+            if ((pixel_index % 96) == 0)
+                 oled_data <= 16'b1111111111111111;//border_colour;
+            else if ((pixel_index % 96) == 95)
+                 oled_data <= 16'b1111111111111111;// border_colour;
+            else if ((pixel_index / 96) == 0)
+                 oled_data <= 16'b1111111111111111;// border_colour;
+            else if ((pixel_index / 96) == 63)
+                 oled_data <= 16'b1111111111111111;// border_colour;       
+             else 
+                oled_data <= 16'b0;
+        end
+        
     end
         
     Oled_Display Oled_Display (clk6p25m, reset, frame_begin, sending_pixels,
--- SoundDisplay/SoundDisplay.srcs/sources_1/new/Top_Student.v
+++ SoundDisplay/SoundDisplay.srcs/sources_1/new/Top_Student.v
@@ -18,7 +18,7 @@
 module Top_Student (
     input J_MIC_Pin3,   // Connect from this signal to Audio_Capture.v
     input CLK100MHZ,
-    input sw,
+    input [1:0] sw,
     input btnC,
     output [7:0] JC,
     output J_MIC_Pin1,
@@ -30,13 +30,13 @@ module Top_Student (
     wire clk625;
     wire reset_active;
     reg [15:0] colour = 16'h07E0;
-    MUX_21 mux_21(sw, mic_in, led);
+    MUX_21 mux_21(sw[0], mic_in, led);
     clk_20k sample_clk(CLK100MHZ, clk20);
     Audio_Capture ac(CLK100MHZ, clk20, J_MIC_Pin3, J_MIC_Pin1, J_MIC_Pin4, mic_in);
     clk_6p25m oled_clk (CLK100MHZ, clk625);
     resetswitch res (clk625, btnC, reset_active);
 //    Oled_Display disp (clk625, reset_active,,,,, {mic_in[11:7], 6'b0, mic_in[11:7]}, JC[0], JC[1], JC[3], JC[4], JC[5], JC[6], JC[7],);
-    DisplayModule DM0 (clk625, JC[0], JC[1], JC[3], JC[4], JC[5], JC[6], JC[7]);
+    DisplayModule DM0 (clk625, JC[0], JC[1], JC[3], JC[4], JC[5], JC[6], JC[7], sw[1]);
     
     
     
